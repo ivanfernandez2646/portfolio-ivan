2af8bfeb45e40dbe612a1c43782a27d4523ca7f1
Ivan Fernandez <ivanfernandez422000@gmail.com>
2021-04-29 14:43:09 +0200

    set up and implemented translations. favicon added

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -1,56 +1,57 @@
+
 const gulp = require('gulp');
 const fileinclude = require('gulp-file-include');
 const server = require('browser-sync').create();
-const { watch, series } = require('gulp');
+const translate = require('gulp-translator');
 
 const paths = {
   scripts: {
-    src: './',
-    dest: './build/',
-    imgDest: './build/assets'
+    src: '../',
+    dest: '../build/',
+    imgDest: '../build/assets'
   }
 };
 
-// Reload Server
 async function reload() {
   server.reload();
 }
 
-// Copy assets after build
 async function copyAssets() {
   gulp.src(['assets/**/*'])
     .pipe(gulp.dest(paths.scripts.imgDest));
 }
 
-// Build files html and reload server
 async function buildAndReload() {
   await includeHTML();
   await copyAssets();
   reload();
 }
 
-async function includeHTML(){
+async function includeHTML() {
+  const env = process.env;
+  const language = (env.LANG || env.LANGUAGE || env.LC_ALL || env.LC_MESSAGES).split('_')[0] || 'en';
+  const translation = `translations/${language}.json`;
+
   return gulp.src([
-    '*.html',
-    '*.css',
+      '*.html',
+      '*.css',
     ])
     .pipe(fileinclude({
       prefix: '@@',
       basepath: '@file',
     }))
+    .pipe(translate(translation))
     .pipe(gulp.dest(paths.scripts.dest));
 }
 exports.includeHTML = includeHTML;
 
-exports.default = async function() {
-  // Init serve files from the build folder
+exports.default = async function () {
   server.init({
     server: {
       baseDir: paths.scripts.dest
     }
   });
-  // Build and reload at the first time
   buildAndReload();
-  // Watch task
-  watch(["*.html","assets/**/*","*.css", "partial/**/*.html"], series(buildAndReload));
+  gulp.watch(["*.html", "*.css", "partial/**/*.html", "assets/**/*", "translations/**/*"], 
+    gulp.series(buildAndReload));
 };
